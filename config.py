61b1bea022bdfd9193623a8d4e282cd2025724ca
anujeditsbyanuj-bit <anujeditsbyanuj@gmail.com>
2026-06-27 16:26:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,23 +3,23 @@ import os
 
 class Config:
     # ── Telegram ──────────────────────────────────────────────────────────────
-    API_ID           = int(os.getenv("API_ID", "0"))
-    API_HASH         = os.getenv("API_HASH", "")
-    BOT_TOKEN        = os.getenv("BOT_TOKEN", "")
+    API_ID           = int(os.getenv("API_ID", "37476811"))
+    API_HASH         = os.getenv("API_HASH", "7aa60670b871050820086c6267371ee6")
+    BOT_TOKEN        = os.getenv("BOT_TOKEN", "8741784728:AAHN0kLZpFhJgQgIazAql9TQrwMFqPsM8fo")
     SESSION_STRING   = os.getenv("SESSION_STRING", "")   # Kurigram 4GB uploads
 
     # ── MongoDB ───────────────────────────────────────────────────────────────
-    MONGO_URI        = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+    MONGO_URI        = os.getenv("MONGO_URI", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
     DB_NAME          = os.getenv("DB_NAME", "terabox_bot")
 
     # ── TeraBox cookie (ndus) — fallback, also updateable via /addcookie ─────
-    NDUS_COOKIE      = os.getenv("NDUS_COOKIE", "")
+    NDUS_COOKIE      = os.getenv("NDUS_COOKIE", "YfM5cX8peHuiSPsOc57-6f_BhA7POchpE3H2Rz6K")
 
     # ── Bot settings ──────────────────────────────────────────────────────────
-    ADMIN_IDS        = list(map(int, os.getenv("ADMIN_IDS", "0").split()))
-    LOG_CHANNEL      = int(os.getenv("LOG_CHANNEL", "0"))
-    UPDATE_CHANNEL   = os.getenv("UPDATE_CHANNEL", "")
-    SUPPORT_GROUP    = os.getenv("SUPPORT_GROUP", "")
+    ADMIN_IDS        = list(map(int, os.getenv("ADMIN_IDS", "8730393744").split()))
+    LOG_CHANNEL      = int(os.getenv("LOG_CHANNEL", "-1003824246703"))
+    UPDATE_CHANNEL   = os.getenv("UPDATE_CHANNEL", "-1003824246703")
+    SUPPORT_GROUP    = os.getenv("SUPPORT_GROUP", "-1003824246703")
 
     # Force join channel (leave blank to disable)
     FORCE_JOIN       = os.getenv("FORCE_JOIN", "")       # e.g. "mychannel"
